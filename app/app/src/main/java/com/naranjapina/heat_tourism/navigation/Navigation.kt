c2bf9847b8f65e7dc2d1f66016a05818a23f6528
Achalogy <achalogy@gmail.com>
2026-03-11 17:27:56 -0500

    Navigation base

--- app/app/src/main/java/com/naranjapina/heat_tourism/navigation/Navigation.kt
+++ app/app/src/main/java/com/naranjapina/heat_tourism/navigation/Navigation.kt
@@ -1,2 +1,22 @@
 package com.naranjapina.heat_tourism.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Screen {
+    Home
+}
+
+@Composable
+fun NavigationStack() {
+    val navController = rememberNavController()
+
+    NavHost (navController, startDestination= Screen.Home.name) {
+        composable(
+            route = Screen.Home.name
+        ) {
+        }
+    }
+}

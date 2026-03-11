abcfde9869fc2c57650edd9c521a7aff886034dd
Achalogy <achalogy@gmail.com>
2026-03-11 17:29:17 -0500

    use navigation stack

--- app/app/src/main/java/com/naranjapina/heat_tourism/MainActivity.kt
+++ app/app/src/main/java/com/naranjapina/heat_tourism/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.Navigation
+import com.naranjapina.heat_tourism.navigation.NavigationStack
 import com.naranjapina.heat_tourism.ui.theme.HeatTourismTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +21,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HeatTourismTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Surface() {
+                NavigationStack()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HeatTourismTheme {
-        Greeting("Android")
     }
 }

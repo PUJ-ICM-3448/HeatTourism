7788dae0629f64684dab51fb5ca9b3a01fc99910
Achalogy <achalogy@gmail.com>
2026-03-11 17:47:52 -0500

    fix navigation

--- app/app/src/main/java/com/naranjapina/heat_tourism/navigation/Navigation.kt
+++ app/app/src/main/java/com/naranjapina/heat_tourism/navigation/Navigation.kt
@@ -44,127 +44,82 @@ enum class Screen {
 fun NavigationStack() {
     val navController = rememberNavController()
 
-    NavHost (navController, startDestination= Screen.Buy.name) {
+    NavHost (navController, startDestination= Screen.LogIn.name) {
         composable(
             route = Screen.Buy.name
         ) {
             BuyScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.CheckIn.name) {
         composable(
             route = Screen.CheckIn.name
         ) {
             CheckInScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Company.name) {
         composable(
             route = Screen.Company.name
         ) {
             CompanyScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.CreatePost.name) {
         composable(
             route = Screen.CreatePost.name
         ) {
             CreatePostScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Home.name) {
         composable(
             route = Screen.Home.name
         ) {
             HomeScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.LogIn.name) {
         composable(
             route = Screen.LogIn.name
         ) {
             LogInScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.LogInCoordinator.name) {
         composable(
             route = Screen.LogInCoordinator.name
         ) {
             LogInCoordinatorScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.ManageCompany.name) {
         composable(
             route = Screen.ManageCompany.name
         ) {
             ManageCompanyScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Map.name) {
         composable(
             route = Screen.Map.name
         ) {
             MapScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Post.name) {
         composable(
             route = Screen.Post.name
         ) {
             PostScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Profile.name) {
         composable(
             route = Screen.Profile.name
         ) {
             ProfileScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Register.name) {
         composable(
             route = Screen.Register.name
         ) {
             RegisterScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Route.name) {
         composable(
             route = Screen.Route.name
         ) {
             RouteScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.RouteGroup.name) {
         composable(
             route = Screen.RouteGroup.name
         ) {
             RouteGroupScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.RouteOverview.name) {
         composable(
             route = Screen.RouteOverview.name
         ) {
             RouteOverviewScreen(navController)
         }
-    }
-
-    NavHost (navController, startDestination= Screen.Searcher.name) {
         composable(
             route = Screen.Searcher.name
         ) {

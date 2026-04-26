46d88364da16ac9795d3ab9ef6bd811447a5ebdf
Achalogy <achalogy@gmail.com>
2026-04-26 11:21:45 -0500

    auth view model

---
+++ app/app/src/main/java/com/naranjapina/heat_tourism/shared/AuthViewModel.kt
@@ -0,0 +1,28 @@
+package com.naranjapina.heat_tourism.shared
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class AuthViewModel : ViewModel() {
+  private val auth : FirebaseAuth = Firebase.auth
+
+  private val _currentUser = MutableStateFlow(auth.currentUser)
+  val currentUser : StateFlow<FirebaseUser?> = _currentUser
+
+  var email by mutableStateOf("")
+    private set
+  var password by mutableStateOf("")
+    private set
+  var isLoading by mutableStateOf(false)
+    private set
+  var feedbackMessage by mutableStateOf<String?>(null)
+    private set
+}

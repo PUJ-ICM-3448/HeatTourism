be3160c7bfa2b384c35b279d2940f85823d23ac2
Valeria Herrera <valeria.herrera.arb@gmail.com>
2026-04-26 19:44:20 -0500

    agregar data class Usuario y enums para autenticacion con Firebase

---
+++ app/app/src/main/java/com/naranjapina/heat_tourism/data/model/Enums.kt
@@ -0,0 +1,16 @@
+package com.naranjapina.heat_tourism.data.model
+
+enum class TipoUsuario {
+    TURISTA,
+    COORDINADOR,
+    ADMIN_EMPRESA
+}
+
+enum class PreferenciaViaje {
+    PLAYA,
+    MONTANIA,
+    CULTURAL,
+    GASTRONOMICO,
+    AVENTURA,
+    NATURALEZA
+}
